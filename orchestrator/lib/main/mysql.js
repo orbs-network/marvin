9afc0f7e9b7ee80756e4122678a92bdc19168c82
Ido Zilberberg <idoz@orbs.com>
2019-09-25 10:34:25 +0300

    Yet another fix

--- orchestrator/lib/main/mysql.js
+++ orchestrator/lib/main/mysql.js
@@ -1,8 +1,10 @@
 const { promisify } = require('util')
 
 function insertTransaction(record = {}, data, connection, tableName, callback) {
+    const hash = (record.commitHash||'').slice(0,6);
+    const version = record.semanticVersion||'';
     connection.query(`INSERT INTO ${tableName} (durationMillis, txResult, rr_createdate, rr_createdate_unix, blockHeight, txId, papiUrl, vchain, commitHash, version)
-                      VALUES (${record.durationMillis}, '${record.txResult}', NOW(), ${Math.floor(Date.now() / 1000)}, ${record.blockHeight}, '${record.txId}', '${record.papiUrl}', '${data.vchain}', '${data.commitHash||''.slice(0,6)}', '${data.semanticVersion||''}');`, (error, results, fields) => {
+                      VALUES (${record.durationMillis}, '${record.txResult}', NOW(), ${Math.floor(Date.now() / 1000)}, ${record.blockHeight}, '${record.txId}', '${record.papiUrl}', '${data.vchain}', '${hash}', '${version}');`, (error, results, fields) => {
 
         if (error != null) {
             throw new Error(error)
