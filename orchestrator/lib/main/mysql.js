3dcf47fba963fc0e0846608ccd8deae579bc37d7
Ido Zilberberg <idoz@orbs.com>
2019-09-25 10:40:11 +0300

    Yet another fix II

--- orchestrator/lib/main/mysql.js
+++ orchestrator/lib/main/mysql.js
@@ -1,10 +1,13 @@
 const { promisify } = require('util')
 
 function insertTransaction(record = {}, data, connection, tableName, callback) {
-    const hash = (record.commitHash||'').slice(0,6);
-    const version = record.semanticVersion||'';
-    connection.query(`INSERT INTO ${tableName} (durationMillis, txResult, rr_createdate, rr_createdate_unix, blockHeight, txId, papiUrl, vchain, commitHash, version)
-                      VALUES (${record.durationMillis}, '${record.txResult}', NOW(), ${Math.floor(Date.now() / 1000)}, ${record.blockHeight}, '${record.txId}', '${record.papiUrl}', '${data.vchain}', '${hash}', '${version}');`, (error, results, fields) => {
+    const hash = (data.commitHash||'').slice(0,6);
+    const version = data.semanticVersion||'';
+
+    const queryStr = `INSERT INTO ${tableName} (durationMillis, txResult, rr_createdate, rr_createdate_unix, blockHeight, txId, papiUrl, vchain, commitHash, version)
+                      VALUES (${record.durationMillis}, '${record.txResult}', NOW(), ${Math.floor(Date.now() / 1000)}, ${record.blockHeight}, '${record.txId}', '${record.papiUrl}', '${data.vchain}', '${hash}', '${version}');`;
+
+    connection.query(queryStr, (error, results, fields) => {
 
         if (error != null) {
             throw new Error(error)
