d125a1b07e6bb86e45c46cdca3006afdf6fd5850
itamar Arjuan <itamar.arjuan@gmail.com>
2019-11-05 13:26:23 +0200

    adding remote deploy script

--- deploy-marvin.sh
+++ deploy-marvin.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -e
 
-LOAD_NVM="export NVM_DIR=\"$HOME/.nvm\" && [ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" && nvm use v10.16.3"
-
 runRemoteCommand(){
     ssh ubuntu@ec2-34-222-245-15.us-west-2.compute.amazonaws.com "cd marvin && $1"
 }
 
 #runRemoteCommand "git pull && ./client-build.sh"
-runRemoteCommand ""
+runRemoteCommand "bash deploy/deploy.sh"
 #runRemoteCommand "$LOAD_NVM && cd orchestrator && npm install"
 #runRemoteCommand "$LOAD_NVM && cd job-executor && npm install"
 #runRemoteCommand "pm2 restart 0"
