c27e9423739d104170eeaa5cef7a58ca085a7a63
Ido Zilberberg <idoz@orbs.com>
2019-11-17 16:21:25 +0200

    Events table has empty strings as default because NULLs confuse Grafana's dashboard annotations.

--- docker/mysql/cre-events.sql
+++ docker/mysql/cre-events.sql
@@ -3,13 +3,13 @@ CREATE TABLE `events`
     `id`               int(11) unsigned NOT NULL AUTO_INCREMENT,
     `event_start`      datetime         NOT NULL,
     `event_end`        datetime     DEFAULT NULL,
-    `vchain`           varchar(50)  DEFAULT NULL,
-    `description`      varchar(200) DEFAULT NULL,
-    `tag1`             varchar(16)  DEFAULT NULL,
-    `tag2`             varchar(16)  DEFAULT NULL,
-    `semantic_version` varchar(50)  DEFAULT NULL,
-    `commit_hash`      varchar(50)  DEFAULT NULL,
+    `vchain`           varchar(50)  DEFAULT '',
+    `description`      varchar(200) DEFAULT '',
+    `tag1`             varchar(16)  DEFAULT '',
+    `tag2`             varchar(16)  DEFAULT '',
+    `semantic_version` varchar(50)  DEFAULT '',
+    `commit_hash`      varchar(50)  DEFAULT '',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
+  AUTO_INCREMENT = 10
   DEFAULT CHARSET = latin1;
